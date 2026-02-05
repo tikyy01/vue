ea82acb2d8c4a800683266fafd9cb3e4f3b692ff
tikyy01 <ervinaksizov@gmail.com>
2026-02-06 01:53:22 +0300

    29

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,34 @@ export default {
   name: 'App',
   data() {
     return {
-      arr: ['x', 'y', 'z']
+      obj: {
+        user1: '100$',
+        user2: '200$',
+        user3: '300$'
+      }
     }
   }
 }
 </script>
 
 <template>
+  <!-- Задание 1 -->
   <ul>
-    <li v-for="(elem, key) in arr" :key="key">
-      {{ key + 1 }}
-    </li>
+    <li v-for="value in obj" :key="value">{{ value }}</li>
+  </ul>
+  
+  <!-- Задание 2 -->
+  <ul>
+    <li v-for="(value, key) in obj" :key="key">{{ key }} - {{ value }}</li>
+  </ul>
+  
+  <!-- Задание 3 -->
+  <ul>
+    <li v-for="(value, key, index) in obj" :key="key">{{ key }} - {{ value }} - {{ index }}</li>
+  </ul>
+  
+  <!-- Задание 4 -->
+  <ul>
+    <li v-for="(value, key, index) in obj" :key="key">{{ key }} - {{ value }} - {{ index + 1 }}</li>
   </ul>
 </template>
