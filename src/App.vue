c144472b7cb5b90c547de62c4688772116686c86
tikyy01 <ervinaksizov@gmail.com>
2026-02-06 01:35:13 +0300

    16

--- src/App.vue
+++ src/App.vue
@@ -3,31 +3,37 @@ export default {
   name: 'App',
   data() {
     return {
-      cost: 100,
-      amount: 3
-    }
-  },
-  computed: {
-    price: function() {
-      return this.cost * this.amount;
+      clicked: false
     }
   },
   methods: {
-    changeCost: function() {
-      this.cost = 150;
+    // Задание 1: Ссылка без перехода
+    handleLinkClick: function(event) {
+      event.preventDefault();
+      alert('Переход по ссылке предотвращен');
+    },
+    
+    // Задание 2: Однократный клик по кнопке
+    handleButtonClick: function() {
+      if (!this.clicked) {
+        alert('Первый и последний клик!');
+        this.clicked = true;
+      }
     }
   }
 }
 </script>
 
 <template>
-  <div>
-    <!-- Вывод данных -->
-    <p>Цена: {{ cost }}</p>
-    <p>Количество: {{ amount }}</p>
-    <p>Общая стоимость: {{ price }}</p>
-    
-    <!-- Кнопка для изменения стоимости -->
-    <button @click="changeCost">Изменить цену на 150</button>
-  </div>
+  <!-- Задание 1 -->
+  <a href="https://example.com" @click="handleLinkClick">
+    Ссылка (клик без перехода)
+  </a>
+  
+  <br><br>
+  
+  <!-- Задание 2 -->
+  <button @click="handleButtonClick">
+    Кнопка (только первый клик)
+  </button>
 </template>
