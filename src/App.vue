dc3fa32264542b8caa279ccf60c36bed2d3a0ab4
tikyy01 <ervinaksizov@gmail.com>
2026-02-06 01:50:20 +0300

    22

--- src/App.vue
+++ src/App.vue
@@ -3,38 +3,13 @@ export default {
   name: 'App',
   data() {
     return {
-      visible1: true,
-      visible2: true,
-      visible3: true
-    }
-  },
-  methods: {
-    toggle1: function() {
-      this.visible1 = !this.visible1;
-    },
-    toggle2: function() {
-      this.visible2 = !this.visible2;
-    },
-    toggle3: function() {
-      this.visible3 = !this.visible3;
+      isAdmin: true
     }
   }
 }
 </script>
 
 <template>
-  <button @click="toggle1">
-    {{ visible1 ? 'Скрыть абзац 1' : 'Показать абзац 1' }}
-  </button>
-  <p v-if="visible1">Абзац 1</p>
-  
-  <button @click="toggle2">
-    {{ visible2 ? 'Скрыть абзац 2' : 'Показать абзац 2' }}
-  </button>
-  <p v-if="visible2">Абзац 2</p>
-  
-  <button @click="toggle3">
-    {{ visible3 ? 'Скрыть абзац 3' : 'Показать абзац 3' }}
-  </button>
-  <p v-if="visible3">Абзац 3</p>
+  <p v-if="isAdmin">Администратор</p>
+  <p v-else>Пользователь</p>
 </template>
