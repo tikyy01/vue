03bc59be9603b7eaff1b9a1f6cc0c0af90e351e8
tikyy01 <ervinaksizov@gmail.com>
2026-02-06 01:47:51 +0300

    17

--- src/App.vue
+++ src/App.vue
@@ -3,37 +3,13 @@ export default {
   name: 'App',
   data() {
     return {
-      clicked: false
-    }
-  },
-  methods: {
-    // Задание 1: Ссылка без перехода
-    handleLinkClick: function(event) {
-      event.preventDefault();
-      alert('Переход по ссылке предотвращен');
-    },
-    
-    // Задание 2: Однократный клик по кнопке
-    handleButtonClick: function() {
-      if (!this.clicked) {
-        alert('Первый и последний клик!');
-        this.clicked = true;
-      }
+      visible: true
     }
   }
 }
 </script>
 
 <template>
-  <!-- Задание 1 -->
-  <a href="https://example.com" @click="handleLinkClick">
-    Ссылка (клик без перехода)
-  </a>
-  
-  <br><br>
-  
-  <!-- Задание 2 -->
-  <button @click="handleButtonClick">
-    Кнопка (только первый клик)
-  </button>
+  <p v-if="visible">Первый абзац (виден при visible=true)</p>
+  <p v-else>Второй абзац (виден при visible=false)</p>
 </template>
