0bab49cd257d27999e1e85d513203db10b55e22a
tikyy01 <ervinaksizov@gmail.com>
2026-02-07 17:43:37 +0300

    35

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,29 @@ export default {
   name: 'App',
   data() {
     return {
-      products: [
-        {id: 1, name: 'product1'},
-        {id: 2, name: 'product2'},
-        {id: 3, name: 'product3'}
-      ]
+      items: ['a', 'b', 'c', 'd', 'e']
+    }
+  },
+  methods: {
+    addItem: function() {
+      this.items.push('новый');
+    },
+    removeFirst: function() {
+      this.items.shift();
+    },
+    removeLast: function() {
+      this.items.pop();
+    },
+    removeSecondLast: function() {
+      if (this.items.length >= 2) {
+        this.items.splice(this.items.length - 2, 1);
+      }
+    },
+    sortItems: function() {
+      this.items.sort();
+    },
+    reverseItems: function() {
+      this.items.reverse();
     }
   }
 }
@@ -15,8 +33,12 @@ export default {
 
 <template>
   <ul>
-    <li v-for="product in products" :key="product.id">
-      {{ product.name }}
-    </li>
+    <li v-for="item in items" :key="item">{{ item }}</li>
   </ul>
+  <button @click="addItem">Добавить в конец</button>
+  <button @click="removeFirst">Удалить первый</button>
+  <button @click="removeLast">Удалить последний</button>
+  <button @click="removeSecondLast">Удалить предпоследний</button>
+  <button @click="sortItems">Сортировать</button>
+  <button @click="reverseItems">Обратный порядок</button>
 </template>
